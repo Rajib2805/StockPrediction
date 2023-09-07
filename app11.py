239c4918978cfb311637d7b805fddd00ac2e68c3
Rajib2805 <68332870+Rajib2805@users.noreply.github.com>
2023-09-07 16:44:46 +0530

    Update app11.py

--- app11.py
+++ app11.py
@@ -15,7 +15,7 @@ from xgboost import XGBRegressor
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.ensemble import ExtraTreesRegressor
 from sklearn.metrics import r2_score, mean_absolute_error
-from PIL import Image
+from Pillow import Image
 import plotly.express as px
 
 ############################################################################################
