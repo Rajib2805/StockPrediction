ae89cb5073f41a0c419abf67e62f9696de6fb0b2
Rajib2805 <68332870+Rajib2805@users.noreply.github.com>
2023-09-07 20:14:04 +0530

    Update app11.py

--- app11.py
+++ app11.py
@@ -41,7 +41,7 @@ def main():
         dataframe()
     elif option == 'Visualize by yourself':
         # Adjust the width of the Streamlit page
-        st.set_page_config(page_title="Use Pygwalker In Streamlit", layout="wide")
+        #st.set_page_config(page_title="Use Pygwalker In Streamlit", layout="wide")
         st.title("Use Pygwalker In Streamlit")
         pyg_html= pyg.walk (df, dark= 'light', return_html=True) # dark= 'light'
         components.html(pyg_html, scrolling=True)
