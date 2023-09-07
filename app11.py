1bc120d1a5e890e996bb0176629c2b62a57deeb1
Rajib2805 <68332870+Rajib2805@users.noreply.github.com>
2023-09-07 21:06:02 +0530

    Update app11.py

--- app11.py
+++ app11.py
@@ -149,7 +149,7 @@ def dataframe():
 
 def streamlit_tableau():
     # Adjust the width of the Streamlit page
-    st.set_page_config(page_title="Use Pygwalker In Streamlit", layout="wide")
+    #st.set_page_config(page_title="Use Pygwalker In Streamlit", layout="wide")
     #st.title("Use Pygwalker In Streamlit")
     pyg_html= pyg.walk (data, dark= 'light', return_html=True) # dark= 'light'
     components.html(pyg_html, scrolling=True)
