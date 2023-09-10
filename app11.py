d7f27cd64ccdc52c5aa08cd01588f954ebb0ed1a
Rajib2805 <68332870+Rajib2805@users.noreply.github.com>
2023-09-10 11:53:46 +0530

    Update app11.py

--- app11.py
+++ app11.py
@@ -19,7 +19,6 @@ from PIL import Image
 import plotly.express as px
 import streamlit.components.v1 as components
 from streamlit_lottie import st_lottie
-import time
 
 ############################################################################################
 # SIDEBAR TITLE and MENU (menu no.-1) (automatic run becaus eit is in the main function)
