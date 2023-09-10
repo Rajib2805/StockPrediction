e01f5db58990a3c1c728a2020235ca1242c10ea2
Rajib2805 <68332870+Rajib2805@users.noreply.github.com>
2023-09-10 11:39:50 +0530

    Update app11.py

--- app11.py
+++ app11.py
@@ -106,6 +106,106 @@ data_added_columns['SMA'] = SMAIndicator(data_added_columns.Close, window=14).sm
 # B) WHAT ACTION TO BE DONE IF THE RADIO BUTTION IS CLICKED
 # C) THE TECHNICAL ANALYSIS CODE DRIVING THOSE ACTIONS
 
+
+def comparison():
+    st.subheader("Stocks Performance Comparison")
+    tickers = stock_df["Company Name"]
+    # dropdown for selecting assets
+    dropdown = st.multiselect('Pick your assets', tickers)
+
+    with st.spinner('Loading...'):  # spinner while loading
+        time.sleep(2)
+        # st.success('Loaded')
+
+    dict_csv = pd.read_csv('StockStreamTickersData.csv', header=None, index_col=0).to_dict()[1]  # read csv file
+    symb_list = []  # list for storing symbols
+    for i in dropdown:  # for each asset selected
+        val = dict_csv.get(i)  # get symbol from csv file
+        symb_list.append(val)  # append symbol to list
+
+    def relativeret(df):  # function for calculating relative return
+        rel = df.pct_change()  # calculate relative return
+        cumret = (1+rel).cumprod() - 1  # calculate cumulative return
+        cumret = cumret.fillna(0)  # fill NaN values with 0
+        return cumret  # return cumulative return
+
+    if len(dropdown) > 0:  # if user selects atleast one asset
+        df = relativeret(yf.download(symb_list, start, end))[
+            'Adj Close']  # download data from yfinance
+        # download data from yfinance
+        raw_df = relativeret(yf.download(symb_list, start, end))
+        raw_df.reset_index(inplace=True)  # reset index
+
+        closingPrice = yf.download(symb_list, start, end)[
+            'Adj Close']  # download data from yfinance
+        volume = yf.download(symb_list, start, end)['Volume']
+        
+        st.subheader('Raw Data {}'.format(dropdown))
+        st.write(raw_df)  # display raw data
+        chart = ('Line Chart', 'Area Chart', 'Bar Chart')  # chart types
+        # dropdown for selecting chart type
+        dropdown1 = st.selectbox('Pick your chart', chart)
+        with st.spinner('Loading...'):  # spinner while loading
+            time.sleep(2)
+
+        st.subheader('Relative Returns {}'.format(dropdown))
+                
+        if (dropdown1) == 'Line Chart':  # if user selects 'Line Chart'
+            st.line_chart(df)  # display line chart
+            # display closing price of selected assets
+            st.write("### Closing Price of {}".format(dropdown))
+            st.line_chart(closingPrice)  # display line chart
+
+            # display volume of selected assets
+            st.write("### Volume of {}".format(dropdown))
+            st.line_chart(volume)  # display line chart
+
+        elif (dropdown1) == 'Area Chart':  # if user selects 'Area Chart'
+            st.area_chart(df)  # display area chart
+            # display closing price of selected assets
+            st.write("### Closing Price of {}".format(dropdown))
+            st.area_chart(closingPrice)  # display area chart
+
+            # display volume of selected assets
+            st.write("### Volume of {}".format(dropdown))
+            st.area_chart(volume)  # display area chart
+
+        elif (dropdown1) == 'Bar Chart':  # if user selects 'Bar Chart'
+            st.bar_chart(df)  # display bar chart
+            # display closing price of selected assets
+            st.write("### Closing Price of {}".format(dropdown))
+            st.bar_chart(closingPrice)  # display bar chart
+
+            # display volume of selected assets
+            st.write("### Volume of {}".format(dropdown))
+            st.bar_chart(volume)  # display bar chart
+
+        else:
+            st.line_chart(df, width=1000, height=800,
+                          use_container_width=False)  # display line chart
+            # display closing price of selected assets
+            st.write("### Closing Price of {}".format(dropdown))
+            st.line_chart(closingPrice)  # display line chart
+
+            # display volume of selected assets
+            st.write("### Volume of {}".format(dropdown))
+            st.line_chart(volume)  # display line chart
+
+    else:  # if user doesn't select any asset
+        st.write('Please select atleast one asset')  # display message
+# Stock Performance Comparison Section Ends Here
+
+
+
+
+
+
+
+
+
+
+
+    
 def tech_indicators():
     st.header('Technical Indicators')
     option = st.radio('Choose a Technical Indicator to Visualize', ['All important indicators', 'BB', 'MACD', 'RSI', 'EMA'])
