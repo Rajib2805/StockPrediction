99b813aa7df8771dcaf89cd6d9feed1cbe2de497
Rajib2805 <68332870+Rajib2805@users.noreply.github.com>
2023-09-11 10:59:41 +0530

    Update app11.py

--- app11.py
+++ app11.py
@@ -39,7 +39,20 @@ img = img.resize((400, 300))
 st.image(img)
 
 #Lottie file for streamlit animation
-st_lottie("https://assets5.lottiefiles.com/packages/lf20_V9t630.json")
+#st_lottie("https://assets5.lottiefiles.com/packages/lf20_V9t630.json")
+
+a= st_lottie("https://assets5.lottiefiles.com/packages/lf20_V9t630.json")
+new_width = 200  # Change this to your desired width
+new_height = 200  # Change this to your desired height
+
+# Update the size properties in the Lottie JSON data
+for layer in a['layers']:
+    if 'w' in layer['ks']:
+        layer['ks']['w']['k'] = new_width
+    if 'h' in layer['ks']:
+        layer['ks']['h']['k'] = new_height
+
+
 
 '''
 #If I want to show the code also on the app, the following code should be used:
