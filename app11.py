6805a7a30c024cd5f295d378572fb07402fbefd6
Rajib2805 <68332870+Rajib2805@users.noreply.github.com>
2023-09-11 11:45:17 +0530

    Update app11.py

--- app11.py
+++ app11.py
@@ -40,10 +40,7 @@ cat = ["bored", "happy", "bored", "bored", "happy", "bored"]
 dog = ["happy", "happy", "happy", "happy", "bored", "bored"]
 activity = ["combing", "drinking", "feeding", "napping", "playing", "washing"]
 
-width = st.sidebar.slider("plot width", 1, 25, 3)
-height = st.sidebar.slider("plot height", 1, 25, 1)
-
-fig, ax = plt.subplots(figsize=(width, height))
+fig, ax = plt.subplots(figsize=(3, 1))
 ax.plot(activity, dog, label="dog")
 ax.plot(activity, cat, label="cat")
 ax.legend()
