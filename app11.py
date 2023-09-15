02b41ebc028770e9c03411acce01503f044a1a41
Rajib2805 <68332870+Rajib2805@users.noreply.github.com>
2023-09-15 17:18:54 +0530

    Update app11.py

--- app11.py
+++ app11.py
@@ -376,19 +376,15 @@ def predictionchart():
     fig.update_layout(height=300, width=400, font_family="Courier New", font_color="blue", title_font_family="Times New Roman", title_font_color="red", legend_title_font_color="green", title_font_size=40)
     st.plotly_chart(fig, theme="streamlit")
     '''
-    fig = px.line(results, title="Time Series with Range Slider and Selectors")
+    fig = px.line(results, title="Chart")
 
-    fig.update_xaxes(      rangeslider_visible=True,     rangeselector=dict(        buttons=list(
-            [
+    fig.update_xaxes(rangeslider_visible=True, rangeselector=dict(buttons=list(
+                [
                 dict(count=1, label="1m", step="month", stepmode="backward"),
                 dict(count=6, label="6m", step="month", stepmode="backward"),
                 dict(count=1, label="YTD", step="year", stepmode="todate"),
                 dict(count=1, label="1y", step="year", stepmode="backward"),
-                dict(step="all"),
-                ]
-            )
-        ),
-    )
+                dict(step="all"),])),)
     fig.update_layout(height=300, width=600)
     fig.update_layout(
     font_family="Courier New",
