5081e82507e7f5e3204f68ee6c5fe38fb64b2a6c
Rajib2805 <68332870+Rajib2805@users.noreply.github.com>
2023-09-15 17:26:02 +0530

    Update app11.py

--- app11.py
+++ app11.py
@@ -366,17 +366,19 @@ def predictionchart():
     df_future['Forecast'] = Y_.flatten()
     df_future['Actual'] = np.nan
     results = pd.concat([df_past, df_future]).set_index('Date')
-    
+   
+   #Visualisation 
     st.write('Prediction of: ', stock)
+    fig.update_layout(height=300, width=600, font_family="Courier New", font_color="blue", title_font_family="Times New Roman", title_font_color="red", legend_title_font_color="green", title_font_size=40))
     fig = px.line(results, title="Chart")
-    fig.update_xaxes(, showgrid=False, rangeslider_visible=True, rangeselector=dict(buttons=list([
+    fig.update_xaxes(showgrid=False, rangeslider_visible=True, rangeselector=dict(buttons=list([
                 dict(count=1, label="1m", step="month", stepmode="backward"),
                 dict(count=6, label="6m", step="month", stepmode="backward"),
                 dict(count=1, label="YTD", step="year", stepmode="todate"),
                 dict(count=1, label="1y", step="year", stepmode="backward"),
                 dict(step="all"),])),)
     fig.update_yaxes(showgrid=True)
-    fig.update_layout(height=300, width=600, font_family="Courier New", font_color="blue", title_font_family="Times New Roman", title_font_color="red", legend_title_font_color="green", title_font_size=40))
+    #fig.update_layout(height=300, width=600, font_family="Courier New", font_color="blue", title_font_family="Times New Roman", title_font_color="red", legend_title_font_color="green", title_font_size=40))
     fig.update_layout(
     font_family="Courier New",
     font_color="blue",
