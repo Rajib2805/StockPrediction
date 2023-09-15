382159b379091298800a1475fd64c3b073917a18
Rajib2805 <68332870+Rajib2805@users.noreply.github.com>
2023-09-15 18:41:38 +0530

    Update app11.py

--- app11.py
+++ app11.py
@@ -325,8 +325,8 @@ def predictionchart():
     y = scaler.transform(y)
 
     # generate the input and output sequences
-    n_lookback = 300  # length of input sequences (lookback period)
-    n_forecast = 30  # length of output sequences (forecast period)
+    n_lookback = 150  # length of input sequences (lookback period)
+    n_forecast = 15  # length of output sequences (forecast period)
 
     X = []
     Y = []
