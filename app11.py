11a4684af8055262912dcf493d9c48c21c02bb60
Rajib2805 <68332870+Rajib2805@users.noreply.github.com>
2023-09-27 22:38:22 +0530

    Update app11.py

--- app11.py
+++ app11.py
@@ -192,6 +192,23 @@ def comparison():
             "url": st.column_config.LinkColumn("App URL"),
              "stars": st.column_config.NumberColumn("Github Stars", help="Number of stars on GitHub", format="%d ⭐",),
             "views_history": st.column_config.LineChartColumn("Views (past 30 days)", y_min=0, y_max=5000),}, hide_index=True,)
+
+
+        ### revised
+        import random
+        dfff = pd.DataFrame({
+           "name": ["Roadmap", "Extras", "Issues"],
+           "url": ["https://roadmap.streamlit.app", "https://extras.streamlit.app", "https://issues.streamlit.app"],
+            "stars": [random.randint(0, 1000) for _ in range(3)],
+            "views_history": [[random.randint(0, 5000) for _ in range(30)] for _ in range(3)],})
+        st.write(dfff)
+        #Calling the data which is set above 
+        st.dataframe(dfff,
+            column_config={
+            "name": "App name",
+            "url": st.column_config.LinkColumn("App URL"),
+             "stars": st.column_config.NumberColumn("Github Stars", help="Number of stars on GitHub", format="%d ⭐",),
+            "views_history": st.column_config.LineChartColumn("Views (past 30 days)", y_min=0, y_max=5000),}, hide_index=True,)
          
 
        
