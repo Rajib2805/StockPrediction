b62cad6f1a0c00e58653f9ac0a859db4e8d8e59b
Rajib2805 <68332870+Rajib2805@users.noreply.github.com>
2023-09-27 21:51:27 +0530

    Update app11.py

--- app11.py
+++ app11.py
@@ -96,13 +96,10 @@ def about():
 
 
     ###Modified
-    ### Original
-    import random
     df = pd.DataFrame({
-        "name": ["Roadmap", "Extras", "Issues"],
-        "url": ["https://roadmap.streamlit.app", "https://extras.streamlit.app", "https://issues.streamlit.app"],
-        "stars": [random.randint(0, 1000) for _ in range(3)],
-        "views_history": [[random.randint(0, 5000) for _ in range(30)] for _ in range(3)],})
+        "name": ["Stock"],
+        "chart": ["https://roadmap.streamlit.app", "https://extras.streamlit.app", "https://issues.streamlit.app"],})
+        
     st.write(df)
     #Calling the data which is set above 
     st.dataframe(df,
