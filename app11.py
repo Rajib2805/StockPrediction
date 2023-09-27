4e85f6f2feb35e479962d028a5d11c902b9877bb
Rajib2805 <68332870+Rajib2805@users.noreply.github.com>
2023-09-27 22:32:35 +0530

    Update app11.py

--- app11.py
+++ app11.py
@@ -176,7 +176,7 @@ def comparison():
         #Volume code
         volume = download_data(symb_list, start_date, end_date)['Volume']
 
-
+        '''
         ### Original
         import random
         df = pd.DataFrame({
@@ -192,7 +192,7 @@ def comparison():
             "url": st.column_config.LinkColumn("App URL"),
              "stars": st.column_config.NumberColumn("Github Stars", help="Number of stars on GitHub", format="%d ⭐",),
             "views_history": st.column_config.LineChartColumn("Views (past 30 days)", y_min=0, y_max=5000),}, hide_index=True,)
-
+         '''
 
        
 
