46b37dc2e06d09a6fcdda6848bb131026ca3f37a
Rajib2805 <68332870+Rajib2805@users.noreply.github.com>
2023-09-27 11:39:57 +0530

    Update app11.py

--- app11.py
+++ app11.py
@@ -137,22 +137,6 @@ data_added_columns['SMA'] = SMAIndicator(data_added_columns.Close, window=14).sm
 # C) THE TECHNICAL ANALYSIS CODE DRIVING THOSE ACTIONS
 
 
-col1, col2, col3 = st.columns(3)
-with col1:
-   st.header("A cat")
-   st.image("https://static.streamlit.io/examples/cat.jpg")
-
-with col2:
-   st.header("A dog")
-   st.image("https://static.streamlit.io/examples/dog.jpg")
-
-with col3:
-   st.header("An owl")
-   st.image("https://static.streamlit.io/examples/owl.jpg")
-
-
-
-
 def comparison():
     stock_df = pd.read_csv("StockStreamTickersData.csv")
     st.subheader("Stocks Performance Comparison")
