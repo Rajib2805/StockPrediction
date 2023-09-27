427f2900d6b7d352d1606d35d03b260b6e75385a
Rajib2805 <68332870+Rajib2805@users.noreply.github.com>
2023-09-27 21:42:16 +0530

    Update app11.py

--- app11.py
+++ app11.py
@@ -96,11 +96,21 @@ def about():
 
 
     ###Modified
+    ### Original
+    import random
+    df = pd.DataFrame({
+        "name": ["Roadmap", "Extras", "Issues"],
+        "url": ["https://roadmap.streamlit.app", "https://extras.streamlit.app", "https://issues.streamlit.app"],
+        "stars": [random.randint(0, 1000) for _ in range(3)],
+        "views_history": [[random.randint(0, 5000) for _ in range(30)] for _ in range(3)],})
+    st.write(df)
     #Calling the data which is set above 
-    st.dataframe(data.Close,
+    st.dataframe(df,
     column_config={
-        "Date": "App name",
-        "Close": st.column_config.LineChartColumn("Views (past 30 days)", y_min=0, y_max=5000),}, hide_index=True,)
+        "name": "App name",
+        "url": st.column_config.LinkColumn("App URL"),
+        "stars": st.column_config.NumberColumn("Github Stars", help="Number of stars on GitHub", format="%d ⭐",),
+        "views_history": st.column_config.LineChartColumn("Views (past 30 days)", y_min=0, y_max=5000),}, hide_index=True,)
 
     
     return()
