bfb1d3a466663b7b62ccf15b26faba835f6c8290
Rajib2805 <68332870+Rajib2805@users.noreply.github.com>
2023-09-27 21:30:36 +0530

    Update app11.py

--- app11.py
+++ app11.py
@@ -83,7 +83,7 @@ def about():
         "url": ["https://roadmap.streamlit.app", "https://extras.streamlit.app", "https://issues.streamlit.app"],
         "stars": [random.randint(0, 1000) for _ in range(3)],
         "views_history": [[random.randint(0, 5000) for _ in range(30)] for _ in range(3)],})
-    
+    st.write(df)
     #Calling the data which is set above 
     st.dataframe(df,
     column_config={
