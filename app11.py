fdad2258c1efef3a7ed18378cb326150e0a218a8
Rajib2805 <68332870+Rajib2805@users.noreply.github.com>
2023-09-27 23:53:05 +0530

    Update app11.py

--- app11.py
+++ app11.py
@@ -195,7 +195,7 @@ def comparison():
 
         ### revised
         dfff = pd.DataFrame({
-             "Date": download_data(symb_list, start_date, end_date)['Date'].tolist()})
+             "Date": download_data(symb_list, start_date, end_date)['Date'].tolist()),
               "close": download_data(symb_list, start_date, end_date)['Adj Close'].tolist()})
         
         #Calling the data which is set above 
