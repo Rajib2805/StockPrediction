f5907d7aa5dac6ac6f280d265946579b931c1bdd
Rajib2805 <68332870+Rajib2805@users.noreply.github.com>
2023-09-27 23:34:06 +0530

    Update app11.py

--- app11.py
+++ app11.py
@@ -196,7 +196,7 @@ def comparison():
         ### revised
         dfff = pd.DataFrame({
            "name": symb_list,
-            "close": closingPrice['Adj Close']})
+            "close": closingPrice.tolist()})
         
         #Calling the data which is set above 
         st.dataframe(dfff,
