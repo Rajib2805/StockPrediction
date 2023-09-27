2f4f4cb7ac0a53650e0d701a184bdccf32900a0c
Rajib2805 <68332870+Rajib2805@users.noreply.github.com>
2023-09-27 13:32:39 +0530

    Update app11.py

--- app11.py
+++ app11.py
@@ -45,7 +45,7 @@ with st.sidebar:
     st_lottie("https://assets5.lottiefiles.com/packages/lf20_V9t630.json")
     
 st.sidebar.info('Welcome to the Stock Price Prediction App. Choose your options below', icon = "🔥")
-st.sidebar.info("Created and designed by Rajib Kumar Tah", icon="ℹ️")
+st.sidebar.info("Created and designed by Rajib Kumar Tah", icon="🤖","ℹ️")
 
 
 
