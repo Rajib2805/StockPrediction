a9e16b91b92b3edcf801457fd191e176ac3df48c
Rajib2805 <68332870+Rajib2805@users.noreply.github.com>
2023-09-27 15:58:02 +0530

    Update app11.py

--- app11.py
+++ app11.py
@@ -296,8 +296,9 @@ def tech_indicators():
 
 def dataframe():
     st.header('Recent Data')
-    st.dataframe(data.tail(100))
-    #st.dataframe(df.style.highlight_max(axis=0))
+    df = data.tail(100)
+    #st.dataframe(data.tail(100))
+    st.dataframe(df.style.highlight_max(axis=0))
 
 
 def streamlit_tableau():
