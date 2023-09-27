8aa4b5c6237dccc4258560ca096ed7f9ec472885
Rajib2805 <68332870+Rajib2805@users.noreply.github.com>
2023-09-27 16:23:48 +0530

    Update app11.py

--- app11.py
+++ app11.py
@@ -76,13 +76,33 @@ def about():
     st.subheader("About")
     st.subheader('Developed by Rajib Kumar Tah')
 
+    ### Original
     import random
     df = pd.DataFrame({
         "name": ["Roadmap", "Extras", "Issues"],
         "url": ["https://roadmap.streamlit.app", "https://extras.streamlit.app", "https://issues.streamlit.app"],
         "stars": [random.randint(0, 1000) for _ in range(3)],
-        "views_history": [[random.randint(0, 5000) for _ in range(30)] for _ in range(3)],
-                     })
+        "views_history": [[random.randint(0, 5000) for _ in range(30)] for _ in range(3)],})
+    
+    #Calling the data which is set above 
+    st.dataframe(df,
+    column_config={
+        "name": "App name",
+        "url": st.column_config.LinkColumn("App URL"),
+        "stars": st.column_config.NumberColumn("Github Stars", help="Number of stars on GitHub", format="%d ⭐",),
+        "views_history": st.column_config.LineChartColumn("Views (past 30 days)", y_min=0, y_max=5000),}, hide_index=True,)
+
+
+
+
+    ###Modified
+    import random
+    df = pd.DataFrame({
+        "name": ["Roadmap", "Extras", "Issues"],
+        "stars": symb_list,
+        "views_history": [[random.randint(0, 5000) for _ in range(30)] for _ in range(3)],})
+    
+    #Calling the data which is set above 
     st.dataframe(df,
     column_config={
         "name": "App name",
