a716d5149d13ffbf68abd68d23d3274b1fa84f93
Rajib2805 <68332870+Rajib2805@users.noreply.github.com>
2023-09-27 22:25:42 +0530

    Update app11.py

--- app11.py
+++ app11.py
@@ -204,14 +204,14 @@ def comparison():
            "url": ["https://roadmap.streamlit.app", "https://extras.streamlit.app", "https://issues.streamlit.app"],
             "stars": [random.randint(0, 1000) for _ in range(3)],
             "views_history": [[random.randint(0, 5000) for _ in range(30)] for _ in range(3)],})
-    st.write(df)
-    #Calling the data which is set above 
-    st.dataframe(df,
-    column_config={
-        "name": "App name",
-        "url": st.column_config.LinkColumn("App URL"),
-        "stars": st.column_config.NumberColumn("Github Stars", help="Number of stars on GitHub", format="%d ⭐",),
-        "views_history": st.column_config.LineChartColumn("Views (past 30 days)", y_min=0, y_max=5000),}, hide_index=True,)
+        st.write(df)
+        #Calling the data which is set above 
+        st.dataframe(df,
+            column_config={
+            "name": "App name",
+            "url": st.column_config.LinkColumn("App URL"),
+             "stars": st.column_config.NumberColumn("Github Stars", help="Number of stars on GitHub", format="%d ⭐",),
+            "views_history": st.column_config.LineChartColumn("Views (past 30 days)", y_min=0, y_max=5000),}, hide_index=True,)
 
 
        
