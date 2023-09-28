7c53b0a02f4f00fc41bc3f527b67b81449962ac3
Rajib2805 <68332870+Rajib2805@users.noreply.github.com>
2023-09-28 13:11:09 +0530

    Update app11.py

--- app11.py
+++ app11.py
@@ -195,7 +195,7 @@ def comparison():
             "views_history": st.column_config.LineChartColumn("Views (past 30 days)", y_min=0, y_max=5000),}, hide_index=True,)
  
         aa = pd.DataFrame({
-           "name": symb_list})
+           "Stock": symb_list})
         st.write(aa)
         
        
