73724cc00fed65d2aff247207293d6dbfcb4a04b
Rajib2805 <68332870+Rajib2805@users.noreply.github.com>
2023-09-29 23:24:10 +0530

    Update app11.py

--- app11.py
+++ app11.py
@@ -70,7 +70,14 @@ st.title('Stock Market Dashboard')
 #st.Image("https://www.pexels.com/photo/close-up-photo-of-monitor-159888/")
 img = Image.open("pexels-leeloo-thefirst-7247399.jpg")
 img = img.resize((400,300))
-st.sidebar.image(img)
+with st.expander("See explanation"):
+    st.write(\"\"\"
+        The chart above shows some numbers I picked for you.
+        I rolled actual dice for these, so they're *guaranteed* to
+        be random.
+    \"\"\")
+    st.sidebar.image(img)
+#st.sidebar.image(img)
 st.sidebar.divider()
 
 #Lottie file for streamlit animation
