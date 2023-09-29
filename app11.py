2e77aff830e966a7861573ed63e0d9eab5bb76c2
Rajib2805 <68332870+Rajib2805@users.noreply.github.com>
2023-09-29 22:07:55 +0530

    Update app11.py

--- app11.py
+++ app11.py
@@ -48,7 +48,13 @@ with st.sidebar:
         selected = option_menu("Utilities", ["FirstMenu", "SecondMenu", "ThirdMenu"])
 
 if(selected == 'FirstMenu'):
-    st.subheader("First Menu job")
+    tab1, tab2, tab3 = st.tabs(["Cat", "Dog"])
+    with tab1:
+       st.header("A cat")
+       st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+    with tab2:
+       st.header("A dog")
+       st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
 elif(selected == 'SecondMenu'):
     st.subheader("Second Menu Job")
 elif(selected == 'ThirdMenu'):
