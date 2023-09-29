a30acb606a96572307adb9c3b6f76a44ca8d5939
Rajib2805 <68332870+Rajib2805@users.noreply.github.com>
2023-09-29 21:45:12 +0530

    Update app11.py

--- app11.py
+++ app11.py
@@ -29,6 +29,7 @@ from sklearn.preprocessing import MinMaxScaler
 from io import BytesIO
 from IPython.display import display, HTML
 import base64
+from streamlit_option_menu import option_menu
 
 #Page configuration settings. Note the icon on the left top corner and the menus on the dot dot dot on the right top corner
 #This settings are to be put before writing any other code in the page just after the library imports are complete
