81e4d2cb0149b28a47a571cbaf07c7563eb3ad99
Rajib2805 <68332870+Rajib2805@users.noreply.github.com>
2023-09-29 23:29:10 +0530

    Update app11.py

--- app11.py
+++ app11.py
@@ -71,11 +71,10 @@ st.title('Stock Market Dashboard')
 img = Image.open("pexels-leeloo-thefirst-7247399.jpg")
 img = img.resize((400,300))
 with st.expander("See explanation"):
-    st.write("\"\"
+    st.write("
         The chart above shows some numbers I picked for you.
         I rolled actual dice for these, so they're *guaranteed* to
-        be random.
-         \"\"\")
+        be random.")
     st.sidebar.image(img)
 #st.sidebar.image(img)
 st.sidebar.divider()
