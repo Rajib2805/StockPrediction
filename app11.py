40816aa3eb93da9294ea86b010bcfa3ee4dd156a
Rajib2805 <68332870+Rajib2805@users.noreply.github.com>
2023-10-09 22:50:31 +0530

    Update app11.py

--- app11.py
+++ app11.py
@@ -47,7 +47,7 @@ st.set_page_config(
 
 
 with st.sidebar:
-    selected = option_menu("Utilities", ["FirstMenu", "SecondMenu", "ThirdMenu"], orientation= 'horizontal')
+    selected = option_menu("Utilities", ["FirstMenu", "SecondMenu", "ThirdMenu"], orientation= 'horizontal',)
 
     if(selected == 'FirstMenu'):
         tab1, tab2 = st.tabs(["📈 Chart", "🗃 Data"])
